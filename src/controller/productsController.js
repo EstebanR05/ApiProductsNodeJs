4ecaa8d73a748bbcc64fcd64798458b3e7f9fc10
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2023-12-13 06:45:33 -0500

    en proceso de la integracion del schema

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,11 +1,13 @@
 const productService = require('../services/productsService');
 
 const getProducts = (req, res) => {
-    const data = productService.getProductsService; 
+    const products = productService.getProductsService(req, res);
+    console.log(products); 
     res.send("this is getProducts");
 };
 
 const ProductPrice = (req, res) => {
+    const servicePrice = productService.ProductPriceService(req, res) 
     res.send("we have in ", req.params.nameProduct, "whith id: ", req.params.id);
 };
 
--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,9 +1,12 @@
-const getProductsService = (req, res) => {
+const productsSchema = require('../schemas/productsSchema')
 
+const getProductsService = (req, res) => {
+    const product = productsSchema.find({});
+    return product;
 };
 
 const ProductPriceService = (req, res) => {
-
+    return;
 };
 
 module.exports = { getProductsService, ProductPriceService };
