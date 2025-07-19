45774fa1dea99a3f1be9d2f20bbca5e5bc9e11f4
Esteban Restrepo <96593734+EstebanR05@users.noreply.github.com>
2025-07-19 14:05:55 -0500

    feat(products): add project data

--- products-typeScript/src/controller/productsController.ts
+++
@@ -1,9 +0,0 @@
-import * as productService from '../services/productsService';
-
-export const getProducts = async (_req: any, res: any) => {
-    return await productService.getProductsService(_req, res);
-};
-
-export const ProductPrice = async (_req: any, res: any) => {
-    return await productService.ProductPriceService(_req, res);
-};
